9831ff5a421313eeb75cac966d7b4bb037ed53be
Lizbethperez <lizbeth_perez_v@hotmail.com>
2018-07-12 21:23:15 -0500

    Subiendo funcion de Validar Numero de tarjeta de Credito Completada y se Completo funcion de Algoritmo de Luhn.

--- app.js
+++ app.js
@@ -19,31 +19,59 @@ const data = {
 // DATOS INGRESADOS POR EL USUARIO.
 
 const nameUser="Carolina Fernandez";
-const cvvUsuer= parseInt("134");
-const expMonthUser=parseInt("09");
-const expYearUser=parseInt("22");
-const creditNumber=parseInt("1235698734560923");
+const cvvUsuer= ("134");
+const expMonthUser=("09");
+const expYearUser=("22");
+const creditNumber=("4791672372586579");
 console.log(creditNumber);
 
 
 const cardNumber =(creditNumber) =>{
-    validateNumbers(creditNumber)
+    validateNumbers(creditNumber) // se manda llamar la validacion que corresponde al numero de tarjeta.
 }
 
 
-const validateNumbers = (dataValidate)=>{
+/*Funcion que valida Que Solo se Ingresen Digitos del 0 al 9 y que sean 16 digitos 
+VALIDA SOLO LAS SIGUIENTES TARJETAS:
+VISA = EMPIEZA CON 4 
+MASTER CARD= EMPIEZA CON 5
+*/
 
-  const regexNumberCard= /^(?:4\d([\- ])?\d{6}\1\d{5}|(?:4\d{3}|5[1-5]\d{2}|6011)([\- ])?\d{4}\2\d{4}\2\d{4})$/;
-  if(!dataValidate.search(regexNumberCard)){
+const validateNumbers = (dataValidate)=>{
+  let arrayCreditNumber=[];
+  const regexNumberCard= /^(?:4\d([\- ])?\d{6}\1\d{5}|(?:4\d{3}|5[1-5]\d{2}|6011)([\- ])?\d{4}\2\d{4}\2\d{4})$/; //Expresion regualar para validar
+  if(regexNumberCard.exec(dataValidate)== null){ // compara la expresion regular con el numero de tarjeta Ingresado.
       console.log("Invalido");
   }else {
       console.log("valido");
+      for(let i=0;i<dataValidate.length;i++){
+        arrayCreditNumber.push(parseInt(dataValidate[i]));
+      }
+      luhnValidate(arrayCreditNumber)   
   }
 
 }
 
-const validateWords = (dataValidate)=>{
- const regex= /^[^a-zA-Z\ \.]$/;
-
+//FUNCION QUE LA TARJETA SE UNA TARJETA VALIDA A TRAVES DEL ALGORITMO DE LUHN 
+  const luhnValidate =(validateCardNumber)=>{
+    let sumDigitsArray=0;
+    let arrayReverseCreditCard=validateCardNumber.reverse();
+    for(let j=0;j<arrayReverseCreditCard.length;j++){
+       let multiplyPar=0;
+       if((j+1)%2===0){
+            multiplyPar=arrayReverseCreditCard[j]*2;
+            if(multiplyPar>=10){
+                let sumDigits=(multiplyPar-10)+1;
+                arrayReverseCreditCard[j]=sumDigits;
+            }else{
+                arrayReverseCreditCard[j]=multiplyPar;
+            }
+       }
+       sumDigitsArray += arrayReverseCreditCard[j];  
+    }
+    if(sumDigitsArray%10===0)
+        console.log('tarjeta valida');
+    else 
+       console.log('tarjeta Invalida');
 }
 cardNumber(creditNumber);
